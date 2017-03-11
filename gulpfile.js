54bedf16ef24f6316163f25a7b36d564ad282b9a
Isravel raj <jeshurunisravel@gmail.com>
2017-03-11 14:54:56 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
-    return gulp.src('./sass/**/*.scss')
+    return gulp.src('./src/assets/scss/**/*.scss')
         .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./src/dummy/css'));
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src('./src/dummy/css/*.css')
+        .pipe(autoprefixer())
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./src/assets/css'));		
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./src/assets/scss/**/*.scss', ['sass']);
+    gulp.watch('./src/dummy/css/**/*.css', ['minify-css']);
 });
