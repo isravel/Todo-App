08b82b0ef0f7698ab43f18066cd27359eb7d68cf
Isravel raj <jeshurunisravel@gmail.com>
2017-04-21 23:18:14 +0530

    Browser-sync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,44 @@
+/*
+npm i -save-dev gulp
+npm i -save-dev gulp-sass
+npm i -save-dev gulp-clean-css
+npm i -save-dev gulp-autoprefixer
+npm i -save-dev browser-sync
+*/
+
+
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
-    return gulp.src('./src/assets/scss/**/*.scss')
+    return gulp.src('./App/scss/**/*.scss')
         .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('./src/dummy/css'));
+});
+
+gulp.task('sass', function() {
+    return gulp.src("./App/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest('./App/dummy/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('minify-css', function() {
-    return gulp.src('./src/dummy/css/*.css')
+    return gulp.src('./App/dummy/css/*.css')
         .pipe(autoprefixer())
         .pipe(cleanCSS())
-        .pipe(gulp.dest('./src/assets/css'));		
+        .pipe(gulp.dest('./App/css'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('./src/assets/scss/**/*.scss', ['sass']);
-    gulp.watch('./src/dummy/css/**/*.css', ['minify-css']);
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./App"
+    });
+    gulp.watch("./App/scss/*.scss", ['sass']);
+    gulp.watch('./App/dummy/css/**/*.css', ['minify-css']);
+    gulp.watch("./App/*.html").on('change', browserSync.reload);
 });
+
+gulp.task('default', ['serve']);
